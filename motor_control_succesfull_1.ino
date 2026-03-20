e4bbbd83b70ef662e558d49834d975f4364a2aee
my-ke20 <michaelotieno915@gmail.com>
2026-03-20 13:34:59 +0300

    Add comments for clarity in motor control code

--- motor_control_succesfull_1.ino
+++ motor_control_succesfull_1.ino
@@ -1,3 +1,4 @@
+//This code uses the L293D motor driver IC to drive a DC motor.
 const int enPin = 5;
 const int in1Pin = 7;
 const int in2Pin = 4;
@@ -15,25 +16,25 @@ void loop()
   if(Serial.available())
   {
     char ch = Serial.read();
-    if(isDigit(ch))
+    if(isDigit(ch)) // Setting the speed of the motor
     {
       int speed = map(ch,'0','9',0,255);
       analogWrite(enPin, speed);
       Serial.println(speed);
     }
-    else if(ch == '+')
+    else if(ch == '+')// Driving the motor clockwise
     {
       Serial.println('CW');
       digitalWrite(in1Pin,LOW);
       digitalWrite(in2Pin,HIGH);
     }
-    else if(ch == '-')
+    else if(ch == '-')// Driving the motor anticlockwise
     {
       Serial.println('ACW');
       digitalWrite(in1Pin,HIGH);
       digitalWrite(in2Pin,LOW);
     }
-    else if(ch != '\n' && ch!= '\r')
+    else if(ch != '\n' && ch!= '\r')// Input error detection section
     {
       Serial.println("No such character ");
       Serial.println(ch);
